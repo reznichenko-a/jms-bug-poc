9b380ab860318d01757a2491880496df99911838
Aleksey Reznichenko <reznichenko_ad@drom.ru>
2025-03-03 18:25:19 +1000

    Add version in code

--- src/index.php
+++ src/index.php
@@ -8,7 +8,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use JMS\Serializer\SerializerBuilder;
 
-$json = '{"name":"Joe Peach","age":29,"array":{"1":"PHP code tester Sandbox Online","Random number":188,"PHP Version":"8.2.20"}}';
+$version = PHP_VERSION;
+
+$json = sprintf('{"name":"Joe Peach","age":29,"array":{"1":"PHP code tester Sandbox Online","Random number":188,"PHP Version":"%s"}}', $version);
 
 $serializer = (new SerializerBuilder())->build();
 
